f4f4d90a2a870b3c7b688216b45924f9094e5d1e
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-08-07 21:36:20 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,96 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import './App.css';
+
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+
+  const [filter, setFilter] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (contacts.find((contact) => contact.name.toLowerCase() === name.toLowerCase())) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+
+    const contact = {
+      id: generateUniqueId(),
+      name,
+      number,
+    };
+
+    setContacts([...contacts, contact]);
+    setName('');
+    setNumber('');
+  };
+
+  const handleDeleteContact = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const generateUniqueId = () => {
+    const nanoid = require('nanoid');
+    return nanoid.nanoid();
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-   goit-react-hw-04-phonebook
+    <div>
+      <h2>Phonebook</h2>
+      <ContactForm
+        name={name}
+        number={number}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </div>
   );
 };
+
+export default App;
